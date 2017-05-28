ba5d9fe9b13a2856c4c86c9fa10f5019fbfef072
NamJuPark <qkrska66@hanyang.ac.kr>
2017-05-28 23:44:53 +0900

    finish

--- app/src/main/java/com/example/mobileapplication11_/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication11_/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.mobileapplication11_;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox checkBox;
@@ -19,9 +29,58 @@ public class MainActivity extends AppCompatActivity {
         myPainter = (MyPainter)findViewById(R.id.myPainter);
     }
 
+    private void checkPer() {//권한 확인 후 수락여부 확인
+        int permissioninfo = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(!(permissioninfo == PackageManager.PERMISSION_GRANTED)){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+        }
+    }
+
+    public String  getExternalPath(){//외부 메모리 쓰기 위해 경로 가져오기
+        String sdPath = "";
+        String ext = Environment.getExternalStorageState();
+        if(ext.equals(Environment.MEDIA_MOUNTED)){
+            sdPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
+        }
+        else{
+            sdPath = getFilesDir()+"mymemo/";
+        }
+        return sdPath;
+    }
+
+    private void makeDir() {//외부 메모리에 디렉토리 만들기
+        String path = getExternalPath();
+        File file = new File(path + "mymemo");
+        file.mkdir();
+        Toast.makeText(this,"디렉토리 생성",Toast.LENGTH_SHORT).show();
+    }
+
+
     private void init() {
         myPainter = (MyPainter)findViewById(R.id.myPainter);
         checkBox = (CheckBox)findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                myPainter.setCheckbox(b);
+                if(myPainter.blur == true){
+                    myPainter.Blur(true);
+                }
+                else{
+                    myPainter.Blur(false);
+                }
+                if(myPainter.color == true) {
+                    myPainter.Color(true);
+                }
+                else{
+                    myPainter.Color(false);
+                }
+            }
+        });
+        checkPer();
+        makeDir();
+
     }
 
     @Override
@@ -37,15 +96,30 @@ public class MainActivity extends AppCompatActivity {
                 myPainter.Eraser();
                 break;
             case R.id.bSave:
-                // myPainter.Save();
+                myPainter.Save(getExternalPath() + "sample.jpg");
                 break;
             case R.id.bOpen:
+                myPainter.Open(getExternalPath() + "sample.jpg");
                 break;
             case R.id.bRotate:
                 checkBox.setChecked(true);
+                myPainter.setCheckbox(true);
+                myPainter.Rotate();
                 break;
             case R.id.bMove:
                 checkBox.setChecked(true);
+                myPainter.setCheckbox(true);
+                myPainter.Trans();
+                break;
+            case R.id.bScale:
+                checkBox.setChecked(true);
+                myPainter.setCheckbox(true);
+                myPainter.Scale();
+                break;
+            case R.id.bSkew:
+                checkBox.setChecked(true);
+                myPainter.setCheckbox(true);
+                myPainter.Skew();
                 break;
         }
     }
@@ -54,14 +128,40 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.blur:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    myPainter.Blur(false);
+                }
+                else{
+                    item.setChecked(true);
+                    myPainter.Blur(true);
+                }
                 break;
             case R.id.color:
+                if(item.isChecked()) {
+                    item.setChecked(false);
+                    myPainter.Color(false);
+                }
+                else{
+                    item.setChecked(true);
+                    myPainter.Color(true);
+                }
                 break;
             case R.id.pen_width:
+                if(item.isChecked()) {
+                    item.setChecked(false);
+                    myPainter.setPenWidth(3);
+                }
+                else{
+                    item.setChecked(true);
+                    myPainter.setPenWidth(5);
+                }
                 break;
             case R.id.pen_color_R:
+                myPainter.setPenColor("red");
                 break;
             case R.id.pen_color_B:
+                myPainter.setPenColor("blue");
                 break;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/mobileapplication11_/MyPainter.java
+++ app/src/main/java/com/example/mobileapplication11_/MyPainter.java
@@ -3,16 +3,22 @@ package com.example.mobileapplication11_;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 /**
@@ -22,33 +28,63 @@ import java.io.IOException;
 public class MyPainter extends View {
     Bitmap mBitmap;//그려질 그림 넣을...
     Canvas mCanvas;
-    Paint mPaint = new Paint();
+    Paint mPaint = new Paint();//그림그릴
+    boolean checkbox, blur, color;
+    int X, Y;
+
     public MyPainter(Context context) {
         super(context);
+        this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
     public MyPainter(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    public void setCheckbox(boolean check) {
+        this.checkbox = check;
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas();
         mCanvas.setBitmap(mBitmap);
         mCanvas.drawColor(Color.YELLOW);
+        mPaint.setStrokeWidth(3);
+
 
     }
-    private void drawStamp(){
-        Bitmap img = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
-        mCanvas.drawBitmap(img,10,10,mPaint);
+
+    private void drawStamp() {
+        Bitmap img = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        mCanvas.drawBitmap(img, X, Y, mPaint);
     }
-    private void Rotate(){
-        mCanvas.rotate(45, this.getWidth()/2, getHeight()/2);
+
+    public void Rotate() {
+        mCanvas.rotate(30, mCanvas.getWidth() / 2, mCanvas.getHeight() / 2);
+        invalidate();
     }
-    public void Eraser(){
-        mBitmap.eraseColor(Color.WHITE);
+
+    public void Trans() {
+        mCanvas.translate(10, 10);
+        invalidate();
+    }
+
+    public void Scale() {
+        mCanvas.scale(1.5f, 1.5f);
+        invalidate();
+    }
+
+    public void Skew() {
+        mCanvas.skew(0.2f, 0);
+        invalidate();
+    }
+
+    public void Eraser() {
+        mBitmap.eraseColor(Color.YELLOW);
         invalidate();
     }
 
@@ -56,6 +92,7 @@ public class MyPainter extends View {
         try {
             FileOutputStream out = new FileOutputStream(file_name);
             mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            Toast.makeText(getContext(),"저장됨",Toast.LENGTH_SHORT).show();
             out.close();
             return true;
         } catch (FileNotFoundException e) {
@@ -65,27 +102,116 @@ public class MyPainter extends View {
         }
         return false;
     }
-    float startX = -1, startY = -1, stopX = -1, stopY = -1;
+
+    public void Open(String filename) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(filename));
+                Eraser();
+                mCanvas.scale(0.5f, 0.5f);
+                mCanvas.drawBitmap(BitmapFactory.decodeFile(filename),mCanvas.getWidth()/2,mCanvas.getHeight()/2,mPaint);
+                br.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(), "File not found", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    public void Blur(boolean check) {
+        if (check == true) {
+            blur = true;
+            if (checkbox == true) {
+                BlurMaskFilter blurMaskFilter = new BlurMaskFilter(50, BlurMaskFilter.Blur.INNER);
+                mPaint.setMaskFilter(blurMaskFilter);
+            } else {
+                blur = false;
+                mPaint.setMaskFilter(null);
+            }
+        } else {
+            blur = false;
+            mPaint.setMaskFilter(null);
+        }
+        invalidate();
+
+    }
+
+    public void Color(boolean check) {
+        if (check) {
+            color = true;
+            if (checkbox) {
+                float[] matrixarray = {
+                        2f, 0f, 0f, 0f, -15f,
+                        0f, 2f, 0f, 0f, -15f,
+                        0f, 0f, 2f, 0f, -15f,
+                        0f, 0f, 0f, 1f, 0f,
+                };
+                ColorMatrix matrix = new ColorMatrix(matrixarray);
+                mPaint.setColorFilter(new ColorMatrixColorFilter(matrix));
+            } else {
+                color = false;
+                mPaint.setColorFilter(null);
+            }
+        } else {
+            color = false;
+            mPaint.setColorFilter(null);
+        }
+        invalidate();
+    }
+
+    public void setPenWidth(int width) { // seunghonice@hanyang.ac.kr
+        if (width == 5) {
+            mPaint.setStrokeWidth(width);
+        } else {
+            mPaint.setStrokeWidth(width);
+        }
+        invalidate();
+    }
+
+    public void setPenColor(String str) {
+        if (str.equals("red")) mPaint.setColor(Color.RED);
+        else mPaint.setColor(Color.BLUE);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        Paint paint = new Paint();//실제 그려서 보여질 곳
-        paint.setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.STROKE);
-        canvas.drawRect(startX, startY, stopX, stopY, paint);
+        if (mBitmap != null) {
+            canvas.drawBitmap(mBitmap, 0, 0, mPaint);
+        }
     }
 
+    int oldX = -1, oldY = -1;
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == event.ACTION_DOWN) {
-            startX = event.getX();
-            startY = event.getY();
-        }
-        else if (event.getAction() == event.ACTION_UP) {
-            stopX = event.getX();
-            stopY = event.getY();
-            invalidate();
+        X = (int) event.getX();
+        Y = (int) event.getY();
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {//누를때
+            oldX = X;
+            oldY = Y;
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {//누르고 움직일 때
+            if (oldX != -1) {
+                if (checkbox == false) {
+                    mCanvas.drawLine(oldX, oldY, X, Y, mPaint);
+                    invalidate();//onDraw부르기
+                    oldX = X;
+                    oldY = Y;
+                }
+            }
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {//땔 때
+            if (oldX != -1) {
+                if (checkbox == false) {//stamp 꺼져 있을 때
+                    mCanvas.drawLine(oldX, oldY, X, Y, mPaint);
+                } else {
+                    drawStamp();
+                }
+                invalidate();
+            }
+            oldX = -1;
+            oldY = -1;
         }
         return true;
+
     }
 }
